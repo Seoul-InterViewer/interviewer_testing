3c31965e4d4019930ae8f02ef4d272b2ab892558
GiToon10100011 <boon10034@gmail.com>
2025-05-15 23:24:55 +0900

    회원가입 기능 추가

--- src/actions/registerAction.ts
+++ src/actions/registerAction.ts
@@ -2,23 +2,31 @@
 
 import supabaseClient from "@/lib/supabase/client";
 
-const loginAction = async (
+const registerAction = async (
   _: any,
   formData: FormData
 ): Promise<{ success: boolean; error?: string }> => {
 
   console.log(formData)
   try {
+    const displayName = formData.get("displayName");
     const email = formData.get("email");
     const password = formData.get("password");
 
-    const { data, error } = await supabaseClient.auth.signInWithPassword({
+    const { data, error } = await supabaseClient.auth.signUp({
       email: email as string,
       password: password as string,
+      options: {
+        data: {
+          displayName: displayName as string,
+        },
+      },
     });
 
+    console.log(error);
+
     if (error) return { success: false, error: error.message };
-    if (!data.user) return { success: false, error: "Account not found" };
+    if (!data.user) return { success: false, error: "Error creating account" };
 
     return { success: true };
   } catch (error) {
@@ -26,5 +34,5 @@ const loginAction = async (
   }
 };
 
-export default loginAction;
+export default registerAction;
 
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import registerAction from "@/actions/registerAction";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -11,8 +12,10 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import { useActionState } from "react";
 
 export default function RegisterPage() {
+  const [state, formAction] = useActionState(registerAction, null);
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <Card className="w-[350px]">
@@ -21,19 +24,20 @@ export default function RegisterPage() {
           <CardDescription>Create a new account</CardDescription>
         </CardHeader>
         <CardContent>
-          <form className="space-y-4">
+          <form action={formAction} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="name">Full Name</Label>
-              <Input id="name" type="text" placeholder="Enter your full name" />
+              <Input id="displayName" name="displayName" type="text" placeholder="Enter your full name" />
             </div>
             <div className="space-y-2">
               <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder="Enter your email" />
+              <Input id="email" name="email"  type="email" placeholder="Enter your email" />
             </div>
             <div className="space-y-2">
               <Label htmlFor="password">Password</Label>
               <Input
                 id="password"
+                name="password"
                 type="password"
                 placeholder="Enter your password"
               />
@@ -42,10 +46,12 @@ export default function RegisterPage() {
               <Label htmlFor="confirmPassword">Confirm Password</Label>
               <Input
                 id="confirmPassword"
+                name="confirmPassword"
                 type="password"
                 placeholder="Confirm your password"
               />
             </div>
+            {state?.error && <p className="text-red-500">{state.error}</p>}
             <Button className="w-full">Register</Button>
             <p className="text-sm text-center text-gray-500">
               Already have an account?{" "}
